8c5e460ef085986a43ccd0b1f053ea1c708120e3
senpooja <poojasen744055@gmail.com>
2022-09-08 18:31:15 +0530

    UC-16-AddressBookUsingAdo.net

---
+++ AddressBookUsingAdo.net/AddressBookTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+
+
+namespace AddressBookTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // [TestMethod]
+        //public void AddPersonDetail()
+        // {
+        //     PersonRepo Repo = new PersonRepo();
+        //     PersonModel person = new PersonModel()
+        //     {
+        //         First_Name = "Rajat",
+        //         Last_Name = "Tiwari",
+        //         Address="Golghar",
+        //         City="Gorakhpur",
+        //         State="UP",
+        //         Zip=224211,
+        //         Phone_Num=871227361,
+        //         Email="Rajat@gmail.com",
+        //         Type="Family"
+        //     };
+        //    var result=Repo.AddPersonDetails(person);
+
+        // }
+        [TestMethod]
+        public void GetPersonDetail()
+        {
+            PersonRepo Repo = new PersonRepo();
+            var person = Repo.GetPersonDetails();
+            Assert.IsTrue(person);
+        }
+        [TestMethod]
+        public void GetSelectedPersonDetail()
+        {
+            PersonRepo Repo = new PersonRepo();
+            var person = Repo.GetSelectedPersonDetails();
+            Assert.IsTrue(person);
+        }
+    }
+}
